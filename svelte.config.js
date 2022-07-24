22ebd55c9c006dd000bc9f1d76d8a80a460b95fb
Ryan Atkinson <mail@ryanatkn.com>
2022-07-24 14:51:26 -0500

    fix paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import adapter from '@sveltejs/adapter-static';
 
-// const dev = process.env.NODE_ENV !== 'production'; // see below
+const dev = process.env.NODE_ENV !== 'production'; // see below
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -11,7 +11,7 @@ export default {
 	},
 	kit: {
 		adapter: adapter(),
-		// paths: dev ? undefined : {base: '/svelte-snake-games'}, // for GitHub pages -- delete this line for top-level domains
+		paths: dev ? undefined : {base: '/svelte-snake-games'}, // for GitHub pages -- delete this line for top-level domains
 		files: {assets: 'src/static'},
 		prerender: {default: true},
 	},

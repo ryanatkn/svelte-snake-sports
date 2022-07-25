169eb3bfb28d863fda000f0d170daae2b6bbe9f2
Ryan Atkinson <mail@ryanatkn.com>
2022-07-25 01:39:13 -0500

    snakes

--- src/lib/games/invincible/InvincibleSnake.svelte
+++ src/lib/games/invincible/InvincibleSnake.svelte
@@ -1,5 +1,73 @@
 <script lang="ts">
-	// TODO SimpleSnake but you can press spacebar to be invincible for a moment
+	// TODO invincible snake: SimpleSnake but you can press spacebar to be invincible for a moment
+
+	// This version is a port of the original React project:
+	// https://ryanatkn.github.io/snake-game
+	// See `$lib/games/simple/SimpleSnake.svelte` for the same thing but simplified.
+
+	import SnakeGame from '$lib/SnakeGame.svelte';
+	import Renderer from '$lib/Renderer.svelte';
+	import {createClock} from '$lib/clock';
+	import {browser} from '$app/env';
+	import Settings from '$lib/Settings.svelte';
+	import Score from '$lib/Score.svelte';
+	// import { createGameLoop } from '$lib/createGameLoop';
+
+	const clock = createClock({running: browser});
+
+	let game: SnakeGame | undefined;
+	$: console.log(`game`, game);
+
+	$: game?.update($clock.dt);
+
+	let showSettings = false;
+
+	// // TODO or pass a block store?
+	// const cancelGameLoop = createGameLoop((dt) => {
+	// 	if (game) {
+	// 		// Run all logic on the game for this time delta (dt).
+	// 		// The update function is expected to be run at least every 30-60fps.
+	// 		game.update(dt);
+	// 	}
+
+	// 	// Tell our renderer to update.
+	// 	// This could be refactored depending on the render method,
+	// 	// but in this case we're telling React to redraw at 60fps, which is not recommended!
+	// 	// renderGame(game);
+	// });
+
+	// onDestroy(cancelGameLoop);
+
+	const onKeydown = (e: KeyboardEvent) => {
+		const {key} = e;
+		if (key === '`') {
+			clock.toggle();
+		}
+	};
 </script>
 
-TODO invincible snake
+<svelte:window on:keydown={onKeydown} />
+
+<div class="OriginalSnake">
+	<SnakeGame bind:this={game} />
+	{#if game}
+		<Renderer {game} />
+		<Score {game} />
+		<button on:click={() => (showSettings = !showSettings)}
+			>{#if showSettings}hide settings{:else}show settings{/if}</button
+		>
+		{#if showSettings}
+			<Settings {game} />
+		{/if}
+	{/if}
+</div>
+
+<style>
+	.OriginalSnake {
+		display: flex;
+		justify-content: center;
+		padding-top: 64px;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>

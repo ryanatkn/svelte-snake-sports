1b7a2def7388b1c6b5292dffd0a46dc9b987bd34
Ryan Atkinson <mail@ryanatkn.com>
2022-08-04 11:37:07 -0500

    improve dimensions

---
+++ src/lib/Dimensions.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type {Dimensions} from '$lib/dimensions';
+	import type {Writable} from 'svelte/store';
+
+	export let dimensions: Writable<Dimensions>;
+
+	let clientWidth = 0;
+	let clientHeight = 0;
+
+	$: if (clientWidth && clientHeight) {
+		$dimensions = {width: clientWidth, height: clientHeight};
+	}
+</script>
+
+<div bind:clientWidth bind:clientHeight />
+
+<style>
+	div {
+		position: fixed;
+		z-index: -99999;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
 	import '@feltcoop/felt/ui/style.css';
 	import {base} from '$app/paths';
-	import {browser} from '$app/env';
 	import {writable} from 'svelte/store';
 
 	import '$lib/style.css';
 	import {setDimensions} from '$lib/dimensions';
+	import Dimensions from '$lib/Dimensions.svelte';
 
-	const dimensions = writable({
-		width: browser ? window.innerWidth : 1,
-		height: browser ? window.innerHeight : 1,
-	});
+	const dimensions = writable({width: 0, height: 0});
 	setDimensions(dimensions);
 </script>
 
@@ -19,8 +16,6 @@
 	<link rel="icon" href="{base}/favicon.png" />
 </svelte:head>
 
-<svelte:window
-	on:resize={() => ($dimensions = {width: window.innerWidth, height: window.innerHeight})}
-/>
+<Dimensions {dimensions} />
 
 <slot />

1da610d3f26ae593366bd90232dd686a6b6c7390
Ryan Atkinson <mail@ryanatkn.com>
2022-08-04 22:11:25 -0500

    add some thoughts about the fps component

--- src/lib/Fps.svelte
+++ src/lib/Fps.svelte
@@ -3,6 +3,14 @@
 
 	export let fps = 0;
 
+	// This might usually be implemented as a store, but it uses context,
+	// so as a store it'd either need to have the `clock` as a dependency
+	// or be restricted to only being created during component initialization,
+	// which is a strange restriction to put on a store.
+	// The syntax sugar is also pretty nice.
+	// The main downside is that binding to the `fps` prop from the parent component
+	// only initializes after mounting.
+
 	const clock = getClock();
 	$: $clock.running && update($clock.dt);
 

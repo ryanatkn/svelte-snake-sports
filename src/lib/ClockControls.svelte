698b0bf7bc06ffbcb37a3f6e5581c13f6eda720f
Ryan Atkinson <mail@ryanatkn.com>
2022-08-11 01:32:32 -0500

    rotate blue to brown hue for buttons

--- src/lib/ClockControls.svelte
+++ src/lib/ClockControls.svelte
@@ -9,3 +9,9 @@
 <button class="icon-button" on:click={() => clock.toggle()} title="[Backtick] toggle clock"
 	>{#if running}⏸{:else}▶️{/if}</button
 >
+
+<style>
+	button {
+		filter: hue-rotate(calc(var(--blue_to_brown_hue_rotation) * 1deg));
+	}
+</style>

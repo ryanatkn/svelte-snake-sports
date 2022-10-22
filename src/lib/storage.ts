5bf2d99343c9e4576784becb821ab68a0c790e9d
Ryan Atkinson <mail@ryanatkn.com>
2022-10-22 14:05:15 -0500

    todo

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,5 +1,8 @@
 import {browser} from '$app/environment';
 
+// TODO BLOCK maybe use `locallyStored` or export a `createStorage` function,
+// returning a store instance that gets saved automatically, abstracting away the key
+
 export const clearLocalStorage = (key: string): void => {
 	if (!browser) return;
 	localStorage.removeItem(key);

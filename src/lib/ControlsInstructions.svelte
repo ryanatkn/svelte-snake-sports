8fc907a37a3cb6e4fc81811abab7a38eb8000ede
Ryan Atkinson <mail@ryanatkn.com>
2022-10-23 22:55:36 -0500

    style tweak

--- src/lib/ControlsInstructions.svelte
+++ src/lib/ControlsInstructions.svelte
@@ -1,5 +1,7 @@
 <h2>controls</h2>
-<div><strong>queue a move</strong>: arrow keys, <code>wasd</code>, <code>hjkl</code></div>
+<div>
+	<strong>queue a move</strong>: <code>arrow keys</code>, <code>wasd</code>, <code>hjkl</code>
+</div>
 <div><strong>move and end turn</strong>: hold <code>shift</code> and move</div>
 <div><strong>toggle clock</strong>: <code>Backtick `</code></div>
 <div><strong>take one turn</strong>: <code>1</code></div>

a691a3c79541719f6018457e9ce148aa1a669c20
Ryan Atkinson <mail@ryanatkn.com>
2022-10-25 11:25:30 -0500

    hold shift

--- src/lib/DirectionalControls.svelte
+++ src/lib/DirectionalControls.svelte
@@ -9,27 +9,27 @@
 <div class="directional-controls">
 	<button
 		class="icon-button"
-		title="queue move left [a, h, left arrow] [end turn with shift or ctrl]"
+		title="queue move left [a, h, left arrow] [hold shift to end turn]"
 		class:selected={selectedDirection === 'left'}
 		on:click={() => select('left')}>←</button
 	>
 	<div class="up-and-down">
 		<button
 			class="icon-button"
-			title="queue move up [w, k, up arrow] [end turn with shift or ctrl]"
+			title="queue move up [w, k, up arrow] [hold shift to end turn]"
 			class:selected={selectedDirection === 'up'}
 			on:click={() => select('up')}>↑</button
 		>
 		<button
 			class="icon-button"
-			title="queue move down [s, j, down arrow] [end turn with shift or ctrl]"
+			title="queue move down [s, j, down arrow] [hold shift to end turn]"
 			class:selected={selectedDirection === 'down'}
 			on:click={() => select('down')}>↓</button
 		>
 	</div>
 	<button
 		class="icon-button"
-		title="queue move right [d, l, right arrow] [end turn with shift or ctrl]"
+		title="queue move right [d, l, right arrow] [hold shift to end turn]"
 		class:selected={selectedDirection === 'right'}
 		on:click={() => select('right')}>→</button
 	>

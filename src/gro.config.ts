9091707fb0b692d0dc46a21427d2983d9bdbb3ed
Ryan Atkinson <mail@ryanatkn.com>
2022-11-10 19:48:30 -0600

    upgrade deps (#31)

--- src/gro.config.ts
+++ src/gro.config.ts
@@ -2,9 +2,11 @@ import type {GroConfigCreator, GroConfigPartial} from '@feltcoop/gro';
 
 const files = ['lib/index.ts', 'lib/style.css'];
 
-export const config: GroConfigCreator = async ({dev}) => {
+const config: GroConfigCreator = async ({dev}) => {
 	const partial: GroConfigPartial = {
 		builds: [dev ? null : {name: 'library', platform: 'node', input: files}],
 	};
 	return partial;
 };
+
+export default config;
